146ccd2f3cd664103a14cdaf0bd6890a8702f6a6
Ethan <30396562+etrant@users.noreply.github.com>
2024-04-03 03:12:17 -0400

    Update narrative.php
    Found an even cleaner way using alternative syntax

--- narrative.php
+++ narrative.php
@@ -1,5 +1,4 @@
-<?php
-function testPage()
+<?php function testPage()
 {
 ?>
     <img src="images/start.jpg" alt="">
@@ -15,33 +14,40 @@ function testPage()
 }
 ?>
 
-<?php
-function generateStory(int $level)
-{
-    switch ($level) {
-        case 0:
-?>
-            <img src="images/start.jpg" alt="">
-            <div class="narrative">
-                <p>
-                    You awaken to find yourself in solitude, a cool breeze brushes lightly against your skin.
-                    Before you stands a dilapidated stone structure, its ancient walls whispering tales of old.
-                    <br><br>
-                    The forest around you hums with life, yet you find yourself strikingly <strong>alone</strong>.
-                </p>
-            </div>
-            <form id="options" method="post" action="game.php">
-                <button class="option" type="submit" name="level_id" value="1">Option 1</button>
-                <button class="option" type="submit" name="level_id" value="2">Option 2</button>
-            </form>
-<?php
-            break;
-        case 1:
-?>
+<?php function generateStory(int $level)
+{ ?>
+    <?php if ($level == 0) : ?>
+        <img src="images/start.jpg" alt="">
+        <div class="narrative">
+            <p>
+                You awaken to find yourself in solitude, a cool breeze brushes lightly against your skin.
+                Before you stands a dilapidated stone structure, its ancient walls whispering tales of old.
+                <br><br>
+                The forest around you hums with life, yet you find yourself strikingly <strong>alone</strong>.
+            </p>
+        </div>
+        <form id="options" method="post" action="game.php">
+            <button class="option" type="submit" name="level_id" value="1">Option 1</button>
+            <button class="option" type="submit" name="level_id" value="2">Option 2</button>
+        </form>
 
-<?php
-        default:
-            testPage();
-    } // closes the switch
-} // closes the function
-?>
+    <?php elseif ($level == 1) : ?>
+    <?php elseif ($level == 2) : ?>
+    <?php elseif ($level == 3) : ?>
+    <?php elseif ($level == 4) : ?>
+    <?php elseif ($level == 5) : ?>
+
+    <?php elseif ($level == 6) : ?>
+    <?php elseif ($level == 7) : ?>
+    <?php elseif ($level == 8) : ?>
+    <?php elseif ($level == 9) : ?>
+    <?php elseif ($level == 10) : ?>
+
+    <?php elseif ($level == 11) : ?>
+    <?php elseif ($level == 12) : ?>
+    <?php elseif ($level == 13) : ?>
+    <?php elseif ($level == 14) : ?>
+    <?php elseif ($level == 15) : ?>
+    <?php else : testPage() ?>
+    <?php endif; ?>
+<?php } ?>
